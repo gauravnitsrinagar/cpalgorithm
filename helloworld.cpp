d1221f0e1fa5fe8c4ae2e5be0e5eac5e2abfa2d4
gaurav chauhan <gauravjnvmau@gmail.com>
2026-04-18 00:42:07 +0530

    this is a test commit message

---
+++ helloworld.cpp
@@ -0,0 +1,38 @@
+#include<bits/stdc++.h>
+#define ll long long 
+#define vi vector<int>
+#define vvi vector<vector<int>>
+#define f(n) for (int i=0;i<n;i++)
+#define g(n) for (int j=i;j<n;j++)
+#define  elif else if
+#define pb push_back
+#define mp make_pair
+#define F first
+#define S second
+#define all(x) x.begin(),x.end()
+#define inf 1e9
+#define mod 1000000007
+#define endl "\n"
+#define sz(x) (int)x.size()
+#define fastio() ios_base::sync_with_stdio(false);cin.tie(NULL);cout.tie(NULL);
+#define test() int t;cin>>t;while(t--)
+#define test1() int t;cin>>t;for(int i=1;i<=t;i++)
+
+using namespace std;
+int main(){
+    int a=9;
+
+      
+
+    cout<<pow(a,3)<<endl;
+    
+
+
+
+
+
+
+
+
+    
+}
